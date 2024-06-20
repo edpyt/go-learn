66f94fd0b8e470acd2aa44afc99bd176f7335bbc
edpyt <edrsa1321s@gmail.com>
2024-06-20 16:26:53 +0300

    e5

---
+++ ultimate_go/slices/example5.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type user struct {
+  likes int
+}
+
+func main() {
+  users := make([]user, 3)
+
+  shareUser := &users[1]
+
+  shareUser.likes++
+
+  for i := range users {
+    fmt.Printf("User: %d Likes: %d\n", i, users[i].likes)
+  }
+
+  users = append(users, user{})
+
+  shareUser.likes++
+
+  println("**************************")
+  for i := range users {
+    fmt.Printf("User: %d Likes: %d\n", i, users[i].likes)
+  }
+}
