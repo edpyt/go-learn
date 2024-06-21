2674fe58bf90b0d7db9446bbb022edb0c88d94e6
edpyt <edrsa1321s@gmail.com>
2024-06-21 15:44:53 +0300

    examples slices

---
+++ ultimate_go/slices/example9.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+  "encoding/binary"
+  "fmt"
+)
+
+func main() {
+  x := []byte{0x0A, 0x15, 0x0e, 0x28, 0x05, 0x96, 0x0b, 0xd0, 0x0}
+
+  a := x[0]
+  b :=  binary.LittleEndian.Uint16(x[1:3])
+  c :=  binary.LittleEndian.Uint16(x[3:5])
+  d :=  binary.LittleEndian.Uint16(x[5:9])
+
+  fmt.Println(a,b,c,d)
+}
