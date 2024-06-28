aa58822bc0212561b19f211c3b3b669d85ef0485
edpyt <edrsa1321s@gmail.com>
2024-06-28 16:12:26 +0300

    decoupling

---
+++ ultimate_go/composition-decoupling/example2.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+  "errors"
+  "fmt"
+  "io"
+  "math/rand"
+  "time"
+)
+
+type Data struct {
+  Line string
+}
+
+type Puller interface {
+  Pull(d *Data) error
+}
+
+type Storer interface {
+  Store(d *Data) error
+}
+
+type Xenia struct {
+  Host string
+  Timeout time.Duration
+}
+
+func (*Xenia) Pull(d *Data) error {
+  switch rand.Intn(10) {
+  case 1, 9:
+    return io.EOF
+  case 5:
+    return errors.New("error reading data from Xenia")
+  default:
+    d.Line = "Data"
+    fmt.Println("In:", d.Line)
+    return nil
+  }
+}
+
+type Pillar struct {
+  Host    string
+  Timeout time.Duration
+}
+
+func (*Pillar) Store(d *Data) error {
+  fmt.Println("Out:", d.Line)
+  return nil
+}
+
+type System struct {
+  Xenia
+  Pillar
+}
+
+func pull(p Puller, data []Data) (int, error) {
+  for i := range data {
+    if err := p.Pull(&data[i]); err != nil {
+      return i, err
+    }
+  }
+
+  return len(data), nil
+}
+
+func store(s Storer, data []Data) (int, error) {
+  for i := range data {
+    if err := s.Store(&data[i]); err != nil {
+      return i, err
+    }
+  }
+
+  return len(data), nil
+}
+
+func Copy(sys *System, batch int) error {
+  data := make([]Data, batch)
+
+  for {
+    i, err := pull(&sys.Xenia, data)
+    if i > 0 {
+      if _, err := store(&sys.Pillar, data[:i]); err != nil {
+        return err
+      }
+    }
+
+    if err != nil {
+      return err
+    }
+  }
+}
+
+func main() {
+  sys := System{
+    Xenia: Xenia{
+      Host:   "localhost:8000",
+      Timeout: time.Second,
+    },
+    Pillar: Pillar{
+      Host:   "localhost:9000",
+      Timeout: time.Second,
+    },
+  }
+
+  if err := Copy(&sys,3); err != io.EOF {
+    fmt.Println(err)
+  }
+}
