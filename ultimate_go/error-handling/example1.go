abd9314cdd10c519f8f6f233209ccae2dbbaf43c
edpyt <edrsa1321s@gmail.com>
2024-06-29 17:52:35 +0300

    e 111e

---
+++ ultimate_go/error-handling/example1.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type error interface {
+  Error() string
+}
+
+type errorString struct {
+  s string
+}
+
+func (e *errorString) Error() string {
+  return e.s
+}
+
+func New(text string) error {
+  return &errorString{text}
+}
+
+func main() {
+  if err := webCall(); err != nil {
+    fmt.Println(err)
+    return
+  }
+  fmt.Println("Life is good")
+}
+
+func webCall() error {
+  return New("bad request")
+}
+
