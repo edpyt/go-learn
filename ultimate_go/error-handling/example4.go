4a6b2c4bf53b1d3e19cad211da71fa2e80089793
edpyt <edrsa1321s@gmail.com>
2024-06-30 09:53:43 +0300

    eee 444

---
+++ ultimate_go/error-handling/example4.go
@@ -0,0 +1,83 @@
+package example4
+
+import (
+  "bufio"
+  "fmt"
+  "io"
+  "log"
+  "net"
+)
+
+type client struct {
+  name string
+  reader *bufio.Reader
+}
+
+func (c *client) TypeAsContext() {
+  for {
+    line, err := c.reader.ReadString("\n")
+    if err != nil {
+      switch e := err.(type) {
+
+      case *net.OpError:
+        if !e.Temporary() {
+          log.Println("Temporary: Client leaving chat")
+          return
+        }
+
+      case *net.AddrError:
+        if !e.Temporary() {
+          log.Println("Temporary: Client leaving chat")
+          return
+        }
+
+      case *net.DNSConfigError:
+        if !e.Temporary() {
+          log.Println("Temporary: Client leaving chat")
+          return
+        }
+
+      default:
+        if err == io.EOF {
+          log.Println("EOF: Client leaving chat")
+          return
+        }
+
+        log.Println("read-routine", err)
+    }
+  }
+
+  fmt.Println(line)
+}
+
+type temporary interface {
+  Temporary() bool
+}
+
+
+// BehaviorAsContext shows how to check for the behavior of an interface
+// that can be returned from the net package.
+func (c *client) BehaviorAsContext() {
+	for {
+		line, err := c.reader.ReadString('\n')
+		if err != nil {
+			switch e := err.(type) {
+			case temporary:
+				if !e.Temporary() {
+					log.Println("Temporary: Client leaving chat")
+					return
+				}
+
+			default:
+				if err == io.EOF {
+					log.Println("EOF: Client leaving chat")
+					return
+				}
+
+				log.Println("read-routine", err)
+			}
+		}
+
+		fmt.Println(line)
+	}
+}
