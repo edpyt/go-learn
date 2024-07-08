71d6878bb4ebf256ba8ea33beccc3c3d17131442
edpyt <edrsa1321s@gmail.com>
2024-07-08 18:26:59 +0300

    e2

---
+++ ultimate_go/channels/example2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  fanOut()
+}
+
+func fanOut() {
+  children := 2000 
+  ch := make(chan string, children)
+
+  for c := 0; c < children; c++ {
+    go func(child int) {
+      time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+      ch <- "data"
+      fmt.Println("child : sent signal :", child)
+    }(c)
+  }
+
+  for children > 0 {
+    d := <-ch
+    children--
+    fmt.Println(d)
+    fmt.Println("parent : recv'd signal :", children)
+  }
+
+  time.Sleep(time.Second)
+
+  fmt.Println("----------------------------------")
+}
