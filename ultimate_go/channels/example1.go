dac415f44203402f062e7b11b8e49bea1a905815
edpyt <edrsa1321s@gmail.com>
2024-07-08 18:26:50 +0300

    e1

---
+++ ultimate_go/channels/example1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+  "fmt"
+  "math/rand"
+  "time"
+)
+
+func main() {
+  waitForResult()
+}
+
+func waitForResult() {
+  ch := make(chan string)
+
+  go func() {
+    time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+    ch <- "data"
+    fmt.Println("child : sent signal")
+  }()
+
+  d := <-ch
+  fmt.Println("parent : recv'd signal :", d)
+
+  time.Sleep(time.Second)
+  
+  fmt.Println("--------------------------------------")
+}
